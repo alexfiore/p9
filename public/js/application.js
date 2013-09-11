0852e6a5a1629da1fcd992ad5fdab035eae688d4
Alex Fiore <writefiore@gmail.com>
2013-09-10 22:48:48 -0700

    added meal

--- public/js/application.js
+++ public/js/application.js
@@ -1,6 +1,19 @@
 $(document).ready(function() {
-  $('.awesomeness_teller').on('click', function() {
+  $('.awesomeness_teller').on('click', function(e) {
+    var awe_some = $(e.target).attr('data-heading');
+    $('#awesomeness_holder').text(awe_some)
+  }); 
 
+
+  $('.skill_teller').on('click', function() {
+    var route = window.location.pathname.split('/');
+    var skill = route.pop();
+    $('#skill_holder').text(skill);
+  });
+
+  $('.meal_teller').on('click', function() {
+    var meal = $('#meal').text();
+    $('#meal_holder').text(meal);
   });
 
   // What other events do you need to bind to make the other pages work?
